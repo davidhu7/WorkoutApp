ab4e9ad961476bb0b4d808ec9316870bfc37a540
SongRe <songks712@gmail.com>
2020-06-23 17:08:21 -0400

    openNewActivity working now :)

--- app/src/main/java/com/example/workoutapp/MainActivity.java
+++ app/src/main/java/com/example/workoutapp/MainActivity.java
@@ -2,17 +2,38 @@ package com.example.workoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        startBtn = (Button) findViewById(R.id.touchToStart);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainScreen();
+            }
+        });
+
     }
+
+    public void openMainScreen () {
+        Intent openMainScreen = new Intent(this, MainScreen.class);
+        startActivity(openMainScreen);
+    }
+
+
 
     
 
